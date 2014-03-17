c426de98d00c1e7f37966d21938e2029b601467b
Alex Van Ischev <blookho@gmail.com>
2014-03-17 15:21:09 +0400

    new table: relations

---
+++ app/views/relations/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @relation, :id, :relation, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
+# encoding: utf-8
+
+
 Weafam::Application.routes.draw do
+
+  resources :relations
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +63,9 @@ Weafam::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+
+
+
+
 end
