0016eeb9ce91514cc21ef368dc8126b418b78d49
Alex Van Ischev <blookho@gmail.com>
2014-03-26 18:02:04 +0400

    передается через сессию увеличивающийся массив дерева в диалоге стартового дерева

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -2,11 +2,6 @@ class StartController < ApplicationController
 
 
 
-
-
-
-
-
   def enter_myself
     @navigation_var = "Navigation переменная - START контроллер/enter_myself метод"
 
@@ -16,6 +11,7 @@ class StartController < ApplicationController
   end
 
   def enter_father
+    @tree_array = session[:tree_array][:value]
     @navigation_var = "Navigation переменная - START контроллер/enter_father метод"
     form_select_fields  # Формирование массивов значений для форм ввода типа select.
 
@@ -23,8 +19,24 @@ class StartController < ApplicationController
   end
 
   def enter_mother
+    @tree_array = session[:tree_array][:value]
     @navigation_var = "Navigation переменная - START контроллер/enter_mother метод"
     form_select_fields  # Формирование массивов значений для форм ввода типа select.
+
+
+  end
+
+
+  def store_profile(id,relation,name,sex)
+
+    @tree_profile_arr = []
+
+    @tree_profile_arr[0] = id              # id
+    @tree_profile_arr[1] = relation        # Relation
+    @tree_profile_arr[2] = name            # Name
+    @tree_profile_arr[3] = sex             # Sex
+
+    return @tree_profile_arr
 
 
   end
@@ -39,22 +51,48 @@ class StartController < ApplicationController
       @user_sex = check_sex_by_name(@user_name) # display sex by name
     end
 
-    redirect_to start_enter_father_path
+    @tree_array = []
+
+    @tree_profile_arr = store_profile(1,nil,@user_name,@user_sex)
+
+    @tree_array << @tree_profile_arr
+
+
+    session[:tree_array] = {:value => @tree_array, :updated_at => Time.current}
+
+
+
+#    redirect_to start_enter_myself_path
+
+     redirect_to start_enter_father_path
+
+
 
   end
 
 
   def father_store
-    @navigation_var = "Navigation переменная - START контроллер/myself_store метод"
+
+    @tree_array = session[:tree_array][:value]
+
+    @navigation_var = "Navigation переменная - START контроллер/father_store метод"
     @father_name = params[:father_name_select] #
     # проверка, действ-но ли введено мужское имя?
     if !@father_name.blank?
+      @user_sex = check_sex_by_name(@father_name) # display sex by name
       if check_sex_by_name(@father_name)
         @father_name_correct = true
       else
         @father_name_correct = false
       end
     end
+
+    @tree_profile_arr = store_profile(2,1,@father_name,@user_sex)
+
+    @tree_array << @tree_profile_arr
+
+
+    session[:tree_array] = {:value => @tree_array, :updated_at => Time.current}
 
     redirect_to start_enter_mother_path
 
@@ -63,11 +101,16 @@ class StartController < ApplicationController
 
 
   def mother_store
-    @navigation_var = "Navigation переменная - START контроллер/myself_store метод"
+
+    @tree_array = session[:tree_array][:value]
+
+
+    @navigation_var = "Navigation переменная - START контроллер/mother_store метод"
 
     @mother_name = params[:mother_name_select] #
     # проверка, действ-но ли введено женское имя?
     if !@mother_name.blank?
+      @user_sex = check_sex_by_name(@mother_name) # display sex by name
       if !check_sex_by_name(@mother_name)
         @mother_name_correct = true
       else
@@ -75,7 +118,15 @@ class StartController < ApplicationController
       end
     end
 
-    redirect_to start_enter_mother_path
+    @tree_profile_arr = store_profile(2,2,@mother_name,@user_sex)
+
+    @tree_array << @tree_profile_arr
+
+
+    session[:tree_array] = {:value => @tree_array, :updated_at => Time.current}
+
+
+    redirect_to "main/_main_display_tree"
 
 
 
