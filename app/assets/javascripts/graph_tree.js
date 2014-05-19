ff69bd8b8eecf56dd8a4dd0e892847c697ac3f53
evg.rz <evg.rz@yandex.ru>
2014-05-19 14:39:56 +0400

    реорганизация структуры

--- app/assets/javascripts/graph_tree.js
+++ app/assets/javascripts/graph_tree.js
@@ -4,40 +4,44 @@
 
 /*
  *  Класс вычисления координат и построения древа
- *  reTree          - конструктор класса
- *  roundTree       - обход древа
- *  sexing          - определение цвета по полу
- *  getPoints       - вычисляем координаты фигур
- *  constructFigure - рисуем фигуры по заданным координатам
- *  constructLines  - рисуем линии по заданным координатам
- *  constructTree   - построение древа (объединение методов getPoints, constructFigure, constructLines)
- *  в целостную конструкцию
+ *  reTree              - конструктор класса
+ *  roundTree           - обход древа
+ *  sexing              - определение цвета по полу
+ *  constructFigures    - рисуем фигуры по заданным координатам
+ *  constructLines      - рисуем линии по заданным координатам
+ *  constructTree       - построение древа (объединение методов constructFigures и constructLines
+ *  в целостную конструкцию)
  */
 
 function reTree(out, obj) {
 
     this.out = out;
     this.obj = obj;
-
+    this.coordinates = new Object();
+    this.coordinates.author = new Object();
     this.kinetic = new reKinetic('tree_canvas', 1000, 1000);
-    this.wcenter = this.kinetic.params.stage.width / 2;
-    this.hcenter = this.kinetic.params.stage.height / 2;
+    this.coordinates.author.width = this.kinetic.params.stage.width / 2 - (this.kinetic.params.rectangle.width / 2);
+    this.coordinates.author.height = this.kinetic.params.stage.height / 2 - (this.kinetic.params.rectangle.height / 2);
     this.roundTree(this.obj, 0);
     this.kinetic.compilation();
 
 }
 
-reTree.prototype.sexing = function (prop) {
-    return prop?'#bfefff':'#fffacd';
+reTree.prototype.sexing = function (sex) {
+    return sex?'#bfefff':'#fffacd';
 }
 
-reTree.prototype.constructFigure = function (properties, relation) {
+reTree.prototype.constructLines = function (properties) {
 
-    switch (relation) {
+}
+
+reTree.prototype.constructTree = function (properties) {
+
+    switch (properties[3]) {
         case 0:     // author
             this.kinetic.drawRect(
-                this.wcenter - (this.kinetic.params.rectangle.width / 2),
-                this.hcenter - (this.kinetic.params.rectangle.height / 2),
+                this.coordinates.author.width,
+                this.coordinates.author.height,
                 this.sexing(properties[4])
             );
             break;
@@ -50,49 +54,49 @@ reTree.prototype.constructFigure = function (properties, relation) {
             break;
         case 2:     // mother
             this.kinetic.drawCircle(
-                10,
+                100,
                 0,
                 this.sexing(properties[6])
             );
             break;
         case 3:     // son
             this.kinetic.drawCircle(
-                20,
+                200,
                 0,
                 this.sexing(properties[6])
             );
             break;
         case 4:     // daughter
             this.kinetic.drawCircle(
-                30,
+                300,
                 0,
                 this.sexing(properties[6])
             );
             break;
         case 5:     // brother
             this.kinetic.drawCircle(
-                40,
+                400,
                 0,
                 this.sexing(properties[6])
             );
             break;
         case 6:     // sister
             this.kinetic.drawCircle(
-                50,
+                500,
                 0,
                 this.sexing(properties[6])
             );
             break;
         case 7:     // husband
             this.kinetic.drawCircle(
-                60,
+                600,
                 0,
                 this.sexing(properties[6])
             );
             break;
         case 8:     // wife
             this.kinetic.drawCircle(
-                70,
+                700,
                 0,
                 this.sexing(properties[6])
             );
@@ -103,32 +107,18 @@ reTree.prototype.constructFigure = function (properties, relation) {
 
 }
 
-reTree.prototype.constructLine = function (properties, relation) {
-
-}
-
-reTree.prototype.constructTree = function (properties, relation) {
-
-    this.constructFigure(properties, relation);
-    this.constructLine(properties, relation);
-
-}
-
 reTree.prototype.roundTree = function (object) {
 
     var props = new Array();
-    var relation;
 
     for(var prop in object) {
         if(object[prop] instanceof Object)
             this.roundTree(object[prop]);
         else
             props[props.length] = object[prop];
-        if(prop == 'relation_id')
-            relation = object[prop];
     }
 
-    this.constructTree(props, relation);
+    this.constructTree(props);
 
 }
 
@@ -143,22 +133,7 @@ reTree.prototype.roundTree = function (object) {
 
 function reKinetic(stageContainer, stageWidth, stageHeight) {
 
-    this.stage = new Kinetic.Stage({                // холст
-        container: stageContainer,
-        width: stageWidth,
-        height: stageHeight,
-        draggable: true
-    });
-
-    this.layers = new Object();                     // слои
-    this.layers.figure = new Kinetic.Layer();
-    this.layers.lines = new Kinetic.Layer();
-
-    this.shapes = new Object();                     // массивы фигур и линий
-    this.shapes.figures = new Array();
-    this.shapes.lines = new Array();
-
-    this.params = new Object();                     // параметры фигур и линий
+    this.params = new Object();                     // параметры холста, фигур и линий
 
     this.params.stage = new Object();               // параметры холста
     this.params.stage.container = stageContainer;
@@ -168,24 +143,44 @@ function reKinetic(stageContainer, stageWidth, stageHeight) {
     this.params.circle = new Object();              // параметры круга
     this.params.circle.radius = 30;
     this.params.circle.stroke = '#333';
-    this.params.circle.strokeWidth = 1;
+    this.params.circle.strokeWidth = 2;
 
     this.params.rectangle = new Object();           // параметры прямоугольника
     this.params.rectangle.width = 100;
     this.params.rectangle.height = 50;
-    this.params.rectangle.stroke = '#333';
-    this.params.rectangle.strokeWidth = 1;
+    this.params.rectangle.stroke = '#900';
+    this.params.rectangle.strokeWidth = 2;
 
     this.params.line = new Object();                // параметры линий
     this.params.line.stroke = 'black';
-    this.params.line.strokeWidth = 1;
+    this.params.line.strokeWidth = 2;
     this.params.line.lineCap = '#333';
     this.params.line.lineJoin = '#333';
+
+    this.stage = new Kinetic.Stage({                // создаем холст
+        name: 'tree_stage',
+        container: this.params.stage.container,
+        width: this.params.stage.width ,
+        height: this.params.stage.height,
+        draggable: true
+    });
+
+    this.layers = new Object();                     // создаем слои
+    this.layers.figures = new Kinetic.Layer({       // слой с фигурам
+        name: 'figures_layer'
+    });
+    this.layers.lines = new Kinetic.Layer({         // слой с линиями
+        name: 'lines_layer'
+    });
+
+    this.shapes = new Object();                     // массивы фигур и линий
+    this.shapes.figures = new Array();              // массив фигур
+    this.shapes.lines = new Array();                // массив линий
 
 }
 
 /*
- * Рисуем линию и записываем его в массив линий
+ * Рисуем линию по заданным параметрам и записываем её в массив линий
  */
 
 reKinetic.prototype.drawLine = function (points) {
@@ -201,7 +196,7 @@ reKinetic.prototype.drawLine = function (points) {
 }
 
 /*
- * Рисуем круг и записываем его в массив фигур
+ * Рисуем круг по заданным параметрам и записываем его в массив фигур
  */
 
 reKinetic.prototype.drawCircle = function (x, y, fill) {
@@ -218,7 +213,7 @@ reKinetic.prototype.drawCircle = function (x, y, fill) {
 }
 
 /*
- * Рисуем прямоугольник и записываем его в массив фигур
+ * Рисуем прямоугольник по заданным параметрам и записываем его в массив фигур
  */
 
 reKinetic.prototype.drawRect = function (x, y, fill) {
@@ -238,13 +233,13 @@ reKinetic.prototype.drawRect = function (x, y, fill) {
 /*
  * Обходим массивы с фигурами и линиями
  * добавляем фигуры и линии к соответствующим слоям
- * добавляем слои на холст
+ * обходим массив со слоями и добавляем слои на холст
  */
 
 reKinetic.prototype.compilation = function () {
 
     for(var figure in this.shapes.figures)
-        this.layers.figure.add(this.shapes.figures[figure]);
+        this.layers.figures.add(this.shapes.figures[figure]);
 
     for(var line in this.shapes.lines)
         this.layers.lines.add(this.shapes.lines[line]);
