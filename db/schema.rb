821abd6b058c14f73715ee3cba96d2482ba43835
Alex Van Ischev <blookho@gmail.com>
2014-05-29 18:01:37 +0400

    Редакции в get_user_tree

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20140529133816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "adminpack"
 
   create_table "names", force: true do |t|
     t.string   "name"
