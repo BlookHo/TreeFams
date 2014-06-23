5b9ce8b954bee0d564b009515edbef8f2488d198
Alexey Medvedev <medvedev.alexey@gmail.com>
2014-06-23 14:04:06 +0400

    Add circle(user_id) AND circle_as_array(user_id) methods to profile instance object

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,11 +1,28 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   belongs_to :name
+  has_many   :trees
 
   attr_accessor :profile_name, :relation_id
+
+  before_save do
+    self.sex_id = name.try(:sex_id)
+  end
 
   def to_name
     name.try(:name).try(:mb_chars).try(:capitalize)
   end
+
+  # На выходе ближний круг для профоля в дереве user_id
+  def circle(user_id)
+    Tree.where(user_id: user_id, profile_id: self.id).includes(:name)
+  end
+
+  # На выходе - массив, аналогичный tree, который у нас сейчас формируется на старте.
+  # [[ user_id, profile_id, name_id, relation_id , is_profile_id , is_name_id , is_sex_id  ]]
+  def circle_as_array(user_id)
+    circle(user_id).map {|t|  [t.user_id, t.profile_id, t.name_id, t.relation_id, t.is_profile_id, t.is_name_id, t.is_sex_id] }
+  end
+
 
 end
--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,3 +1,4 @@
 class Tree < ActiveRecord::Base
   belongs_to :profile
+  belongs_to :name, foreign_key: "is_name_id"
 end
