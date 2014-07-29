2ded05a6e5ab9454a66f1e81742ed33e0d30e0a1
alex_ubuntu <zoneiva@gmail.com>
2014-07-29 19:22:56 +0400

    update for connecting users

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,33 @@ class User < ActiveRecord::Base
     profile.name.name.capitalize
   end
 
+  # Получение массива соединенных Юзеров
+  # для заданного "стартового" Юзера
+  #
+  def get_connected_users
+    connected_users_arr = []
+    connected_users_arr << self.id
+    first_users_arr = ConnectedUser.where(user_id: self.id).pluck(:with_user_id)
+    if first_users_arr.blank?
+      first_users_arr = ConnectedUser.where(with_user_id: self.id).pluck(:user_id)
+    end
+    one_connected_users_arr = first_users_arr
+    if !one_connected_users_arr.blank?
+      connected_users_arr << one_connected_users_arr
+      connected_users_arr.flatten!.uniq! if !connected_users_arr.blank?
+      one_connected_users_arr.each do |conn_arr_el|
+        next_connected_users_arr = ConnectedUser.where("(user_id = #{conn_arr_el} or with_user_id = #{conn_arr_el})").pluck(:user_id, :with_user_id)
+        if !next_connected_users_arr.blank?
+          one_connected_users_arr << next_connected_users_arr
+          one_connected_users_arr.flatten!.uniq! if !one_connected_users_arr.blank?
+          connected_users_arr << next_connected_users_arr
+          connected_users_arr.flatten!.uniq! if !connected_users_arr.blank?
+        end
+      end
+    end
+    return connected_users_arr
+  end
+
   private
 
   def self.create_with_email(email)
