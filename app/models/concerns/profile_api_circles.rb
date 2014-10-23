c962572f7b85382d0b57e67380247c5afca0b820
Alexey Medvedev <medvedev.alexey@gmail.com>
2014-10-23 17:36:46 +0400

    Api start search fix

--- app/models/concerns/profile_api_circles.rb
+++ app/models/concerns/profile_api_circles.rb
@@ -5,10 +5,39 @@ module ProfileApiCircles
   def circles(current_user)
     user_ids = current_user.get_connected_users
 
-    results = ProfileKey.where(user_id: user_ids, profile_id: self.id).where("relation_id < ?", 9).order('relation_id').includes(:name).to_a.uniq(&:is_profile_id)
-
     # Центральный профиль
-    center_node = {
+    zero_circle = zero_circle(current_user)
+
+    # Первый (ближний круг)
+    second_circle = get_circle(user_ids: user_ids, profile_id: self.id)
+
+
+    # Форматирование
+    # Ближний круг центрального профиля
+    circles = profile_keys_to_hash(second_circle, circle: 1, current_user: current_user)
+
+    # Добавляем центральный профиль на ПЕРВУЮ ПОЗИЦИЮ
+    # Центральный профиль (первая нода - центральная)
+    circles.unshift(zero_circle)
+
+    # Из второго круга исключаются профили, которые в первом круге
+    except_ids =  second_circle.map {|r| r.is_profile_id }.push(self.id)
+
+
+    # Для кождого профиля первого круга собирается его ближний круг, исключаю профили в предыдущем круге
+    second_circle.each do |key|
+      ad_results = get_circle(user_ids: user_ids, profile_id: key.is_profile_id, except_ids: except_ids)
+      circles << profile_keys_to_hash(ad_results, circle: 2, target: key.is_profile_id, current_user: current_user)
+    end
+
+    return circles.flatten!
+  end
+
+
+
+  # Center profile
+  def zero_circle(current_user)
+    {
       id: self.id,
       name: self.name.name,
       relation: "Центр",
@@ -17,27 +46,17 @@ module ProfileApiCircles
       current_user_profile: current_user.profile_id == self.id,
       icon: self.icon_path
     }
-
-    # Ближний круг центрального профиля
-    circles = profile_keys_to_hash(results, circle: 1, current_user: current_user)
-
-    # Центральный профиль (первая нода - центральная)
-    circles.unshift(center_node)
-
-    # Из второго круга исключаются профили, которые в первом круге
-    except_ids =  results.map {|r| r.is_profile_id }.push(self.id)
+  end
 
 
-    # Для кождого профиля первого круга собирается его ближний круг, исключаю профили в предыдущем круге
-    results.each do |key|
-      ad_results = ProfileKey.where(user_id: user_ids, profile_id: key.is_profile_id)
-                             .where("relation_id < ?", 9)
-                             .where.not(is_profile_id: except_ids)
-                             .order('relation_id').includes(:name).to_a.uniq(&:is_profile_id)
-      circles << profile_keys_to_hash(ad_results, circle: 2, target: key.is_profile_id, current_user: current_user)
-    end
 
-    return circles.flatten!
+
+  def get_circle(user_ids: user_ids, profile_id: profile_id, except_ids: [])
+    ProfileKey.where(user_id: user_ids, profile_id: profile_id)
+              .where("relation_id < ?", 9)
+              .where.not(is_profile_id: except_ids)
+              .order('relation_id')
+              .includes(:name).to_a.uniq(&:is_profile_id)
   end
 
 
