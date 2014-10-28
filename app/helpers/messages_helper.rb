62d5643c8ccd84dceef36959ed0d42e5a267640a
alex_ubuntu <zoneiva@gmail.com>
2014-10-28 12:14:23 +0300

    show count new unread  messages in header

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -3,8 +3,8 @@ module MessagesHelper
 
   # подсчет новых, полученных current_user, непрочитанных сообщений
   def count_new_messages  #
-    new_messages_count = Message.where(receiver_id: current_user.id, read:false, receiver_deleted: false).count if current_user
-    return new_messages_count
+    @new_messages_count = Message.where(receiver_id: current_user.id, read:false, receiver_deleted: false).count if current_user
+    return @new_messages_count
   end
 
 
