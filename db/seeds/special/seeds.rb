4eb7d7b565cf8f12fc0ecdbb7dccb9a84ae885ba
alex_ubuntu <zoneiva@gmail.com>
2014-11-12 16:30:44 +0300

    seeds.rb - reset pk added

---
+++ db/seeds/special/seeds.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+
+User.delete_all       #
+User.reset_pk_sequence
+
+Tree.delete_all       #
+Tree.reset_pk_sequence
+
+Profile.delete_all       #
+Profile.reset_pk_sequence
+
+ProfileKey.delete_all       #
+ProfileKey.reset_pk_sequence
+
+ConnectedUser.delete_all       #
+ConnectedUser.reset_pk_sequence
+
+Message.delete_all       #
+Message.reset_pk_sequence
+
+ConnectionRequest.delete_all       #
+ConnectionRequest.reset_pk_sequence
+
