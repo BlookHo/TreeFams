2f9ad2391fc5c1b838b335fba23bb3801526166a
Alex Van Ischev <blookho@gmail.com>
2014-11-25 16:59:24 +0300

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20141029144116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "adminpack"
 
   create_table "connected_users", force: true do |t|
     t.integer  "user_id"
@@ -22,16 +23,6 @@ ActiveRecord::Schema.define(version: 20141029144116) do
     t.boolean  "connected",    default: false
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "connection_requests", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "with_user_id"
-    t.integer  "confirm"
-    t.boolean  "done",          default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "connection_id"
   end
 
   create_table "messages", force: true do |t|
