24963c13a206fa6d8c30e73cab45367e65e5b82a
alex_ubuntu <zoneiva@gmail.com>
2014-12-18 18:36:19 +0300

    new updates  seeds

--- db/seeds/updates/seeds.rb
+++ db/seeds/updates/seeds.rb
@@ -6,16 +6,22 @@ UpdatesEvent.reset_pk_sequence
 UpdatesEvent.create([
 
 
-{name: 'Запрос на объединение', image: 'updates/connect_request.png'},
-{name: 'Объединение', image: 'updates/connection_tree.png'},
-{name: 'Выбор избранного', image: 'updates/fav_choose.png'},
-{name: 'Добавлен профиль', image: 'updates/add_profile.png'},
-{name: 'Приглашение на сайт', image: 'updates/invitation.png'},
+{name: ' отправлен запрос на объединение деревьев от пользователя по имени ', image: 'updates/connect_request.png'},
+{name: ' проведено объединение с ', image: 'updates/connection_tree.png'},
+{name: ' пометил как избранного профиль с именем ', image: 'updates/fav_choose.png'},
+{name: ' в дерево родни добавлен новый профиль с именем ', image: 'updates/add_profile.png'},
+{name: ' на сайт приглашен новый пользователь по имени ', image: 'updates/invitation.png'},
 {name: 'Новые данные в профиле родственнике', image: 'updates/new_profile_data.png'},
 {name: 'Событие у родственника', image: 'updates/family_event.png'},
-{name: 'Количество родни (размер дерева) превысило 25 человек', image: 'updates/tree_vol_25.png'},
-{name: 'Количество родни (размер дерева) превысило 50 человек', image: 'updates/tree_vol_50.png'},
-{name: 'Количество родни (размер дерева) превысило 100 человек', image: 'updates/tree_vol_100.png'}
+{name: ' Количество родни в твоем дереве (размер дерева) превысило 10 человек!', image: 'updates/tree_vol_10.png'}, # № 8 -  10
+{name: ' Количество родни в твоем дереве (размер дерева) превысило 15 человек!', image: 'updates/tree_vol_15.png'}, # 9-  15
+{name: ' Количество родни в твоем дереве (размер дерева) превысило 20 человек!', image: 'updates/tree_vol_20.png'}, # 10-  20
+{name: ' Количество родни в твоем дереве (размер дерева) превысило 25 человек!', image: 'updates/tree_vol_25.png'}, # 11-  25
+{name: ' Количество родни в твоем дереве (размер дерева) превысило 30 человек!', image: 'updates/tree_vol_30.png'}, # 12-  30
+{name: ' Количество родни в твоем дереве (размер дерева) превысило 40 человек!', image: 'updates/tree_vol_40.png'}, # 13-  40
+{name: ' Количество родни в твоем дереве (размер дерева) превысило 50 человек!', image: 'updates/tree_vol_50.png'}, # 14-  50
+{name: ' Количество родни в твоем дереве (размер дерева) превысило 100 человек!', image: 'updates/tree_vol_100.png'},  # 15-  100
+{name: ' Количество родни в твоем дереве (размер дерева) превысило 150 человек!', image: 'updates/tree_vol_150.png'}  # 16-  150
 
 
 
