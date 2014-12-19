c416accaeae80ff284ba76b9081444902973a207
Alexey Medvedev <medvedev.alexey@gmail.com>
2014-12-19 15:07:02 +0300

    Add admin url to slack notification

--- app/models/concerns/slack.rb
+++ app/models/concerns/slack.rb
@@ -3,7 +3,7 @@ module Slack
 
 
   def send_new_pending_user_message_to_slack
-    system(' curl -X POST --data-urlencode \'payload={"channel": "#general", "username": "WeAllFamily Bot", "text": "Новый пользователь ожидает модерации. Для утверждения зайдит в административную панель сайта.", "icon_emoji": ":ghost:"}\' https://hooks.slack.com/services/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 ')
+    system(' curl -X POST --data-urlencode \'payload={"channel": "#general", "username": "WeAllFamily Bot", "text": "Новый пользователь ожидает модерации. Для утверждения зайдите в административную панель сайта. http://weallfamily.ru/admin/pending_users", "icon_emoji": ":ghost:"}\' https://hooks.slack.com/services/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 ')
   end
 
   # curl -X POST --data-urlencode 'payload={"channel": "#general", "username": "WeAllFamily Bot", "text": "Новый пользователь ожидает модерации. Для утверждения, <http://weallfamily.ru/admin/pending_users|нажмите здесь>", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
