cc1d7b39dbad3d14d7348f8661f32efa3c7f970f
Alexey Medvedev <medvedev.alexey@gmail.com>
2015-02-06 18:08:02 +0300

    Switch similars alert to flash link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,8 +9,13 @@ class HomeController < ApplicationController
     tree_info, sim_data, similars = current_user.start_similars
     @log_connection_id = current_tree_log_id(tree_info[:connected_users]) unless tree_info.empty?
     unless similars.empty?  # т.е. есть похожие
-      flash.now[:alert] = "Предупреждение: В дереве есть 'похожие' профили. Объединиться будет невозможно - /home/index" unless similars.empty?
-      # flash.now[:link] = {type: :alert, message: "test message", link: root_path}
+      # flash.now[:alert] = "Предупреждение: В дереве есть 'похожие' профили. Объединиться будет невозможно - /home/index" unless similars.empty?
+      flash_obj = {
+        type: :alert,
+        message: "Предупреждение: В дереве есть 'похожие' профили. Объединиться будет невозможно",
+        link: internal_similars_search_path
+      }
+      flash.now[:link] = flash_obj unless similars.empty?
       unless sim_data.empty?  #  т.е. есть новые похожие - отлич. от ранее записанных
         @tree_info = tree_info  # To View
         view_tree_data(tree_info, sim_data) unless @tree_info.empty?
