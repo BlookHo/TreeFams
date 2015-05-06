9c71324eff627497d906ca8f10caf18f60abb178
Aleksei Ivanishchev <blookho@gmail.com>
2015-05-06 16:39:34 +0300

    Add common_logs to trigger.sql

--- db/trigger.sql
+++ db/trigger.sql
@@ -80,3 +80,9 @@ ON connection_requests
 FOR EACH ROW
 EXECUTE PROCEDURE notify_trigger();
 
+-- trigger for common_logs table
+CREATE TRIGGER watched_table
+AFTER INSERT OR UPDATE OR DELETE
+ON common_logs
+FOR EACH ROW
+EXECUTE PROCEDURE notify_trigger();
