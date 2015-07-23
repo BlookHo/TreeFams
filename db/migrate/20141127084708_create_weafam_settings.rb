60596333124d2fae30e1a6015d26d4202dcb2cb8
Alexey Medvedev <meduza@MedvedevPro.local>
2015-07-23 16:05:03 +0300

    Add WeafamSetting on migrate

--- db/migrate/20141127084708_create_weafam_settings.rb
+++ db/migrate/20141127084708_create_weafam_settings.rb
@@ -2,8 +2,12 @@ class CreateWeafamSettings < ActiveRecord::Migration
   def change
     create_table :weafam_settings do |t|
       t.integer :certain_koeff, :null => false
-
       t.timestamps
     end
+
+    s = WeafamSetting.new
+    s.certain_koeff = 4
+    s.save
+
   end
 end
