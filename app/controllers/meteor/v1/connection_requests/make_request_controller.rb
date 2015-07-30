e4704b634b0e3affb16dbea2270ddcd2e053c3e0
BlookHo <blookho@gmail.com>
2015-07-30 12:07:57 +0300

    add search for connection contr-agent - method

--- app/controllers/meteor/v1/connection_requests/make_request_controller.rb
+++ app/controllers/meteor/v1/connection_requests/make_request_controller.rb
@@ -15,19 +15,9 @@ module Meteor
           status_code = 200 # status Ok
           msg, msg_code = ConnectionRequest.make_request(@current_user, with_user_id)
 
-          
-          # Start search for conn_request Contr-agent
-          certain_koeff = WeafamSetting.first.certain_koeff
-          logger.info "In SearchResultsController: certain_koeff = #{certain_koeff}, @current_user.id = #{@current_user.id}  "
-          ## ЗАПУСК ПОИСКА
-          with_user_id.start_search(certain_koeff)
-          logger.info "In SearchResultsController: After start_search: @error = #{@error}  " if @error
-          logger.info "In SearchResultsController: After start_search"
+          start_counter_search(with_user_id)
 
           # respond_with yes_con
-
-          # logger.info "In MakeRequestController: @error = #{@error}" if @error
-          # logger.info "In MakeRequestController: After connect:  @current_user.id = #{@current_user.id}"
           logger.info "In MakeRequestController: After connect:  msg_code = #{msg_code}, msg = #{msg} "
           status_code = 600 if msg_code > 1 # status NotOk
 
@@ -38,6 +28,23 @@ module Meteor
           end
 
         end
+
+
+        # @note: Start search for conn_request Contr-agent
+        def start_counter_search(with_user_id)
+
+          certain_koeff = WeafamSetting.first.certain_koeff
+          logger.info "In MakeRequestController: certain_koeff = #{certain_koeff} "
+          with_user = User.find(with_user_id)
+          logger.info "In MakeRequestController: with_user_id = #{with_user_id} "
+
+          ## ЗАПУСК ПОИСКА
+          with_user.start_search(certain_koeff)
+          logger.info "In MakeRequestController: After start_search: @error = #{@error}  " if @error
+          logger.info "In MakeRequestController: After start_search"
+
+        end
+
 
 
       end
