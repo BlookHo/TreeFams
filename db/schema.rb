c5d2c2ebcfa9413a1bcc53beb700723167aba279
Alexey Medvedev <medvedev.alexey@gmail.com>
2015-08-04 15:32:59 +0300

    [fix]

--- db/schema.rb
+++ db/schema.rb
@@ -139,8 +139,8 @@ ActiveRecord::Schema.define(version: 20150803133526) do
     t.string   "birthday"
     t.string   "country"
     t.string   "city"
-    t.integer  "deleted",         default: 0
     t.string   "avatar_mongo_id"
+    t.integer  "deleted",         default: 0
   end
 
   create_table "profile_keys", force: true do |t|
