2c4db3e538504146b1424db2c4bb52a842f12297
Aleksei Ivanishchev <author@ema.il>
2015-09-11 18:28:51 +0300

    in schedule - counters started in production

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,8 +20,8 @@ every 2.minutes do
 
   runner "WeafamStat.create_stats_row"  #  , environment: :development
 
-  runner "Counter.increment_invites"     , environment: :development
-  runner "Counter.increment_disconnects" , environment: :development
+  runner "Counter.increment_invites"    # , environment: :development
+  runner "Counter.increment_disconnects"# , environment: :development
 
 end
 
