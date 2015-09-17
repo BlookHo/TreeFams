e8953e55372b2d18d1f27118b3a2506eb19a7ce1
BlookHo <blookho@gmail.com>
2015-09-17 14:19:34 +0300

    Update deploy.rb to rem and update crontab file

--- config/deploy.rb
+++ config/deploy.rb
@@ -97,6 +97,7 @@ set :whenever_identifier,   ->{ fetch :application }
 
 namespace :whenever do
   task :start, :roles => :app do
+    run "cd #{release_path} && crontab -r"
     run "cd #{release_path} && bundle exec whenever --update-crontab"
   end
 end
