790911e57a4c93324188e6786108b8f330a59405
Aleksei Ivanishchev <author@ema.il>
2015-09-17 14:32:28 +0300

    fix in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -98,6 +98,7 @@ set :whenever_identifier,   ->{ fetch :application }
 namespace :whenever do
   task :start, :roles => :app do
     run "cd #{release_path} && crontab -r"
+    run "crontab -r"
     # run "cd #{release_path} && bundle exec whenever --update-crontab"
     run "bundle exec whenever --update-crontab"
   end
