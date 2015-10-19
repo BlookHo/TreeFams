f877d56800a8fca2609c13a22e1cc946fedd1fe3
Aleksei Ivanishchev <author@ema.il>
2015-10-19 17:41:59 +0300

    hash_work for search  refact - phase 1

--- lib/added_classes/hash_work/hash_work.rb
+++ lib/added_classes/hash_work/hash_work.rb
@@ -13,10 +13,10 @@ class HashWork
   # 79=>62 - найти те эл-ты in hash_2, кот-е отс-ют в hash_1
   # add 79=>62 to hash_1
   # Result: {72=>58, 75=>59, 76=>61, 77=>60, 78=>57, 79=>62} /
-  def self.add_to_hash(hash_1,hash_2)
-    arr_key1 = hash_1.keys
-    hash_2.each do |k,v|
-      hash_1 = hash_1.merge!( k => v) if !arr_key1.include?(k)
+  def self.add_to_hash(hash_one,hash_two)
+    arr_key = hash_one.keys
+    hash_two.each do |key,val|
+      hash_one = hash_one.merge!( key => val ) unless arr_key.include?(key)
     end
   end
 
@@ -34,17 +34,18 @@ class HashWork
         next if k == key
         intersection = start_hash[key] & start_hash[k]
         if duplicates_many_to_one.has_key?(key)
-          duplicates_many_to_one[key][intersection.keys.first] = intersection[intersection.keys.first] if !intersection.empty?
+          first_key = intersection.keys.first
+          duplicates_many_to_one[key][first_key] = intersection[first_key] unless intersection.empty?
         else
-          duplicates_many_to_one[key] = intersection if !intersection.empty?
+          duplicates_many_to_one[key] = intersection unless intersection.empty?
         end
       end
     end
 
     # Collect uniqs
-    duplicates_many_to_one.each do |key, value|
-      value.each do |k, v|
-        uniqs[key].delete_if { |kk,vv|  kk == k && vv = v }
+    duplicates_many_to_one.each do |dup_key, value|
+      value.each do |k, value_v|
+        uniqs[dup_key].delete_if { |kk,vv|  kk == k && vv = value_v }
       end
     end
     return uniqs, duplicates_many_to_one
@@ -86,34 +87,84 @@ class HashWork
   # @param admin_page [Integer] опциональный номер страницы
   # @see Place = main_contrl.,
   ################# FILLING OF HASH WITH KEYS AND/OR VALUES
-  # def self.fill_arrays_in_hash(one_hash, tree, profile, relation) # Filling of hash with keys and values, according to key occurance
   def self.fill_arrays_in_hash(fill_arrays_data) # Filling of hash with keys and values, according to key occurance
-
     one_hash = fill_arrays_data[:profiles_hash]
     tree     = fill_arrays_data[:tree_user_id]
     profile  = fill_arrays_data[:tree_profile_id]
     relation = fill_arrays_data[:row_relation_id]
 
-    test_tree = one_hash.key?(tree) # Is profile_searched in one_hash?
-    if test_tree == false #  "key = profile_searched YET NOT in hash - make new hash in hash"
-      one_hash.merge!(tree => { profile => [relation] } ) # include new profile_searched with new profile with new array in hash
-    else
+    # test: one_hash.key?(tree) - Did profile_searched in one_hash?
+    if one_hash.key?(tree) #== true
+      # "key = profile_searched IS in hash - collect hash"
       current_hash = one_hash.fetch(tree) # get hash for tree
-      test_profile_found = current_hash.key?(profile) # Is  elem in one_hash?
-      if test_profile_found == false #  "key=profile NOT Found in hash"
-        current_hash.merge!({profile => [relation]}) # include profile with new array in hash
-        one_hash.merge!(tree => current_hash ) # наполнение хэша соответствиями найденных профилей и найденных отношений
-      else  #  "Found in hash"
-        value_array = current_hash.values_at(profile)
-        value_array << relation
-        value_array = value_array.flatten(1)
-        current_hash.merge!(profile => value_array ) # наполнение хэша соответствиями найденных профилей и найденных отношений
-        one_hash.merge!(tree => current_hash ) # наполнение хэша соответствиями найденных профилей и найденных отношений
-      end
+      test_profile_found = current_hash.key?(profile) # Is elem in one_hash?
+      collect_hash_data = {
+        current_hash: current_hash,
+        one_hash:     one_hash,
+        profile:      profile,
+        tree:         tree,
+        relation:     relation
+      }
+      # PREV REFACT VER
+      # if test_profile_found == false #  "key=profile NOT Found in hash"
+      #   collect_hash_data[:one_data] = [relation] # include profile with new array in hash
+      #   # current_hash.merge!({profile => [relation]}) # include profile with new array in hash
+      #   # one_hash.merge!(tree => current_hash ) # наполнение хэша соответствиями найденных профилей и найденных отношений
+      # else  #  "Found in hash"
+      #   collect_hash_data[:relation] = relation
+      #   value_array = make_relation_value(collect_hash_data)
+      #   # value_array = current_hash.values_at(profile)
+      #   # value_array << relation
+      #   # value_array = value_array.flatten(1)
+      #   collect_hash_data[:one_data] = value_array
+      #   # current_hash.merge!(profile => value_array )
+      #   # one_hash.merge!(tree => current_hash )
+      # end
+
+      new_collect_hash_data = proceed_hash_data(test_profile_found, collect_hash_data)
+      # наполнение хэша соответствиями найденных профилей и найденных отношений
+      one_hash = collect_one_hash(new_collect_hash_data)
+    else  # if !test_tree # == false
+      # "key = profile_searched YET NOT in hash - make new hash in hash"
+      # include new profile_searched with new profile with new array in hash
+      one_hash.merge!(tree => { profile => [relation] } )
     end
     one_hash
   end
 
+  # @note: calc data for hash collecting in dependence of found results
+  def self.proceed_hash_data(test_profile_found, collect_hash_data)
+    if test_profile_found #  "Found in hash"
+      value_array = make_relation_value(collect_hash_data)
+      collect_hash_data[:one_data] = value_array
+    else  # "key=profile NOT Found in hash"
+      collect_hash_data[:one_data] = [collect_hash_data[:relation]] # include profile with new array in hash
+    end
+    collect_hash_data
+  end
+
+  # @note: make_relation_value
+  # used to: fill_arrays_in_hash
+  def self.make_relation_value(collect_hash_data)
+    value_array = collect_hash_data[:current_hash].values_at(collect_hash_data[:profile])
+    value_array << collect_hash_data[:relation]
+    value_array.flatten(1)
+  end
+
+  # @note: Collect hash for found profiles in found trees
+  # used to: fill_arrays_in_hash
+  # наполнение хэша соответствиями найденных профилей и найденных отношений
+  def self.collect_one_hash(collect_hash_data)
+    current_hash = collect_hash_data[:current_hash]
+    one_hash     = collect_hash_data[:one_hash]
+    profile      = collect_hash_data[:profile]
+    tree         = collect_hash_data[:tree]
+    one_data     = collect_hash_data[:one_data]
+    current_hash.merge!(profile => one_data ) # наполнение хэша соответствиями найденных профилей и найденных отношений
+    one_hash.merge!(tree => current_hash ) # наполнение хэша соответствиями найденных профилей и найденных отношений
+  # puts "one_hash = #{one_hash} "
+    one_hash
+  end
 
   # @note: Делаем ХЭШ профилей-отношений для искомого дерева. - пригодится.
   #   Tested
