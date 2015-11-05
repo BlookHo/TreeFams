40ac5e503b6f3722efece266068ea2d4c3c936c9
Aleksei Ivanishchev <author@ema.il>
2015-11-05 11:39:09 +0300

    Weafam_stat rspec add factory fix

--- spec/factories/weafam_settings.rb
+++ spec/factories/weafam_settings.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
 
-    factory :weafam_setting, class: WeafamSetting  do |f|
+  factory :weafam_setting, class: WeafamSetting  do
+    certain_koeff 4
+  end
+
+  factory :weafam_setting_model_test, class: WeafamSetting  do |f|
       f.certain_koeff { Faker::Number.number(1) }
     end
 
--- spec/models/weafam_setting_spec.rb
+++ spec/models/weafam_setting_spec.rb
@@ -9,23 +9,28 @@ RSpec.describe WeafamSetting, type: :model do   # , focus: true
     }
 
     it "has a valid factory" do
-      puts " Model WeafamSetting validation - has a valid factory"
+      puts " Model WeafamSetting validation - has a valid certain_koeff"
       expect(FactoryGirl.create(:weafam_setting)).to be_valid
+    end
+
+    it "has a valid factory" do
+      puts " Model WeafamSetting validation - has a valid factory"
+      expect(FactoryGirl.create(:weafam_setting_model_test)).to be_valid
     end
 
     it "is invalid without a certain_koeff" do
       puts " Model WeafamSetting validation - invalid without a certain_koeff"
-      expect(FactoryGirl.build(:weafam_setting, certain_koeff: nil)).to_not be_valid
+      expect(FactoryGirl.build(:weafam_setting_model_test, certain_koeff: nil)).to_not be_valid
     end
 
     it "is invalid with certain_koeff = 0" do
       puts " Model WeafamSetting validation - invalid with certain_koeff = 0"
-      expect(FactoryGirl.build(:weafam_setting, certain_koeff: 0)).to_not be_valid
+      expect(FactoryGirl.build(:weafam_setting_model_test, certain_koeff: 0)).to_not be_valid
     end
 
     it "is invalid with certain_koeff > 9" do
       puts " Model WeafamSetting validation - invalid with certain_koeff > 9"
-      expect(FactoryGirl.build(:weafam_setting, certain_koeff: 10)).to_not be_valid
+      expect(FactoryGirl.build(:weafam_setting_model_test, certain_koeff: 10)).to_not be_valid
     end
 
   end
