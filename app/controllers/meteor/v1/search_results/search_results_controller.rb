1aa1259b3831e985712f315a61ca8e00cbf3a8f2
Aleksei Ivanishchev <author@ema.il>
2015-11-20 13:20:42 +0300

    search methods start update

--- app/controllers/meteor/v1/search_results/search_results_controller.rb
+++ app/controllers/meteor/v1/search_results/search_results_controller.rb
@@ -8,8 +8,8 @@ module Meteor
         def search
           Thread.new do
             ActiveRecord::Base.connection_pool.with_connection do
-              certain_koeff = WeafamSetting.first.certain_koeff
               if @current_user.start_similars[:similars].blank?
+                certain_koeff = WeafamSetting.first.certain_koeff
                 @current_user.start_search(certain_koeff)
               end
             end
