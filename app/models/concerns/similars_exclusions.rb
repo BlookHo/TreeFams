f215f6cf77990412f581391e304e8ece855d2396
Aleksei Ivanishchev <author@ema.il>
2016-01-14 17:24:37 +0300

    similars exclusions - update

--- app/models/concerns/similars_exclusions.rb
+++ app/models/concerns/similars_exclusions.rb
@@ -16,7 +16,7 @@ module SimilarsExclusions
       inter_relations = common_of_uncommons(uncommon_hash_a, uncommon_hash_b)
       sim_exlude_relations = [ "Отец", "Мама", "Дед-о", "Дед-м", "Бабка-о","Бабка-м", "Брат", "Сестра",
                                "Муж", "Жена", "Сын", "Дочь", "Внук-о", "Внук-м", "Внучка-о", "Внучка-м"   ]
-      # todo: поместить и брать массив exlude_relations из таблицы WeafamSettings
+      # todo: поместить и брать массив exclude_relations из таблицы WeafamSettings
       unsimilar_sign = check_relations_exclusion(inter_relations, sim_exlude_relations)
       logger.info "*** In check_similars_exclusion: unsimilar_sign: #{unsimilar_sign}, inter_relations: #{inter_relations}"
       return unsimilar_sign, inter_relations
@@ -27,8 +27,8 @@ module SimilarsExclusions
     def get_uncommons(a_profile_circle, b_profile_circle, common_hash)
       uncommon_hash_a = unintersection(a_profile_circle, common_hash)
       uncommon_hash_b = unintersection(b_profile_circle, common_hash)
-      #logger.info "*** In get_uncommons 74: uncommon_hash_a: #{uncommon_hash_a}"
-      #logger.info "*** In get_uncommons 75: uncommon_hash_b: #{uncommon_hash_b}"
+      #logger.info "*** In get_uncommons : uncommon_hash_a: #{uncommon_hash_a}"
+      #logger.info "*** In get_uncommons : uncommon_hash_b: #{uncommon_hash_b}"
       return uncommon_hash_a, uncommon_hash_b
     end
 
@@ -39,7 +39,7 @@ module SimilarsExclusions
       relations_b = uncommon_hash_b.keys
       # inter_relations = relations_a & relations_b
       relations_a & relations_b # return inter_relations
-      #logger.info "*** In common_of_uncommons 76: inter_relations: #{inter_relations}"
+      #logger.info "*** In common_of_uncommons : inter_relations: #{inter_relations}"
      # inter_relations
     end
 
@@ -53,8 +53,8 @@ module SimilarsExclusions
       unsimilar_sign = true # Исх.знач-е - сначала считаем похожими
       inter_relations.each do |relation|
         unsimilar_sign = false if exlude_relations.include?(relation) # Значит - точно непохожие
-        # logger.info "*** In check_rels_ each 77-1: relation: #{relation}, exlude_relations.include?(relation) = #{exlude_relations.include?(relation)}, exlude_relations = #{exlude_relations} "
-        # logger.info "*** In check_relations_exclusion 77-2: unsimilar_sign: #{unsimilar_sign}"
+        # logger.info "*** In check_rels_ each 1: relation: #{relation}, exlude_relations.include?(relation) = #{exlude_relations.include?(relation)}, exlude_relations = #{exlude_relations} "
+        # logger.info "*** In check_relations_exclusion 2: unsimilar_sign: #{unsimilar_sign}"
       end
       unsimilar_sign  # передача значения признака (true/false)
     end
